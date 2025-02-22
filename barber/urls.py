5623989ea104d12950a82df32e7b68f848b28f27
Skokov  Roman <skokov161@gmail.com>
2025-02-22 11:23:47 +0300

    вью для промотра зписей на услуги, + шаблон, исправил директорию статик, добавил изображение по дефолту мастерам

--- barber/urls.py
+++ barber/urls.py
@@ -19,11 +19,13 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from core import views
+from core.views import VisitListView
 
 urlpatterns = (
     [   path("", views.IndexView.as_view(), name="index"),
         path("admin/", admin.site.urls),
         path("thanks/", views.ThanksView.as_view(), name="thanks"),
+        path('visits/', VisitListView.as_view(), name='visit_list'),
     ]
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -1,8 +1,13 @@
+
 from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.views.generic.edit import CreateView
 from .models import Master, Service, Visit
 from .forms import VisitForm
-from django.views.generic import View, TemplateView
+from django.views.generic import ListView
+from django.shortcuts import redirect
+from django.db.models import Q
+from .models import Visit, Master
 
 MENU = [
     {'title': 'Главная', 'url': '/', 'active': True},
@@ -12,6 +17,10 @@ MENU = [
     # {'title': 'Оставить отзыв', 'url': '/review/create/', 'active': True},
     {'title': 'Запись на стрижку', 'url': '#orderForm', 'active': True},
 ]
+
+# Импорт базовой вьюхи
+from django.views.generic import View, TemplateView
+
 
 # # Создание класса-наследника View
 # class ThanksView(View):
@@ -23,36 +32,67 @@ MENU = [
 #             'menu': MENU
 #         }
 #         return render(request, 'thanks.html', context)
+    
+
 
 class ThanksView(TemplateView):
     template_name = 'thanks.html'
+    extra_context = {'menu': MENU}
+
+
+class IndexView(CreateView):
+    template_name = 'main.html'
+    form_class = VisitForm
+    success_url = 'thanks'
+    model = Visit
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['menu'] = MENU
+        context['masters'] = Master.objects.all()
+        context['services'] = Service.objects.all()
         return context
-    
 
-class IndexView(View):
-    def get(self, request):
-        context = {
-            'menu': MENU,
-            'masters': Master.objects.all(),
-            'services': Service.objects.all(),
-            'form': VisitForm()
-        }
-        return render(request, 'main.html', context)
+from django.views.generic import ListView
+from django.shortcuts import redirect
+from django.db.models import Q
+from .models import Visit, Master
 
-    def post(self, request):
-        form = VisitForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('thanks')
-        
-        context = {
-            'menu': MENU,
-            'masters': Master.objects.all(),
-            'services': Service.objects.all(),
-            'form': form
-        }
-        return render(request, 'main.html', context)
+
+class VisitListView(ListView):
+    model = Visit
+    template_name = 'visit_list.html'
+    context_object_name = 'visits'
+    paginate_by = 1
+
+    def get_queryset(self):
+        """Формируем QuerySet с учетом поиска и фильтрации по мастеру"""
+        queryset = Visit.objects.all().order_by('-created_at')
+
+        # Получаем параметры из GET-запроса
+        search_query = self.request.GET.get('q', '')
+        master_id = self.request.GET.get('master', '')
+
+        # Фильтрация по поисковому запросу (имя или телефон)
+        if search_query:
+            queryset = queryset.filter(Q(name__icontains=search_query) | Q(phone__icontains=search_query))
+
+        # Фильтрация по мастеру
+        if master_id:
+            queryset = queryset.filter(master_id=master_id)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        """Добавляем в контекст список мастеров и текущие фильтры"""
+        context = super().get_context_data(**kwargs)
+        context['masters'] = Master.objects.all()
+        context['search_query'] = self.request.GET.get('q', '')
+        context['selected_master'] = self.request.GET.get('master', '')
+        return context
+
+    def dispatch(self, request, *args, **kwargs):
+        """Запрещаем доступ не-администраторам"""
+        if not request.user.is_staff:
+            return redirect('main_page')  # Перенаправляем на главную
+        return super().dispatch(request, *args, **kwargs)
